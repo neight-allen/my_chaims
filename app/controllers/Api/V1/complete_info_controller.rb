0f45614ec171d1f72855ffbd40c922b1bdcedd9c
Andrew Carmer <carmerandrew@gmail.com>
2015-12-14 08:31:13 -0700

    fix render json for complete info

--- app/controllers/Api/V1/complete_info_controller.rb
+++ app/controllers/Api/V1/complete_info_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::CompleteInfoController < ApplicationController
   respond_to :json
   def show
     @song = Song.find_by!(id: params[:id])
-    respond_with @song, include: [:album, :artist]
+    render json: @song, include: [:album, :artist]
   end
 end
